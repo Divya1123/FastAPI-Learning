e6250910b1e692a22047c49c7df1171ef1c58af0
Divya_Gusain <38239188+Divya1123@users.noreply.github.com>
2026-08-21 19:08:33 +0530

    Add business requirements and API structure comments
    Added initial comments outlining business requirements and API expectations.

--- 1-First-api.py
+++ 1-First-api.py
@@ -1,3 +1,19 @@
+'''
+Business Requirement: Customer want to have a functionality that classifies the user ticket before reaching their helpdesk.
+We will build a simple API for this functionality. Below would be the expected payload and response of our API.
+
+Request Payload:
+{
+    "ticket_text": "I have received a damaged order. I need replacement."
+}
+
+Response Body expected:
+{
+    "category": "Replacement Ticket",
+    "Summary": "Customer received a damaged product, they are requested replacement."
+}
+'''
+
 from fastapi import FastAPI
 import uvicorn
 
